3723ae2836e08c995cd6248156fd00f998e04377
Matt Oquist <moquist@majen.net>
2015-02-23 11:37:41 -0500

    Add missing :test profile dep on ring-clj-params.
    This makes the tests pass; otherwise they don't.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject fogus/ring-edn "0.3.0-SNAPSHOT"
   :description "A Ring middleware that augments :params by parsing a request body as Extensible Data Notation (EDN)."
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:test {:dependencies [[ring-clj-params "0.1.0"]]}})
